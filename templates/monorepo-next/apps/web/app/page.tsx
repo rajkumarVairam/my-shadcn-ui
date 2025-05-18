4ff75c9e1ab5e0619fb847aa71c5f213dccc2772
rajkumarVairam <raj.vrajkumar1987@gmail.com>
2025-05-18 01:10:36 -0500

    Page.tsx clean up

--- templates/monorepo-next/apps/web/app/page.tsx
+++ templates/monorepo-next/apps/web/app/page.tsx
@@ -1,6 +1,3 @@
-import { Button } from "@workspace/ui/components/button"
-import { Label } from "@workspace/ui/components/label"
-import { Input } from "@workspace/ui/components/input"
 import { LoginForm } from "@workspace/ui/components/login-form"
 
 export default function Page() {
